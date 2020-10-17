a9065ad69f3d9a5abe23964a293ff4df997eead6
Visakhsebastian <37535440+Visakhsebastian@users.noreply.github.com>
2020-10-17 15:20:31 +0530

    Update and rename pgm.md to factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,10 @@
 # Python program to find the factorial of a number provided by the user.
 
+# change the value for a different result
+num = 7
 
+# To take input from the user
+#num = int(input("Enter a number: "))
 
 factorial = 1
 
