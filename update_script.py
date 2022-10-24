6228080b8cfad0e6a34a43a7803932733f45055d
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-24 15:57:47 +0530

    script added for auto update the members data

---
+++ update_script.py
@@ -0,0 +1,30 @@
+import json
+from github import Github
+import argparse
+
+arg = argparse.ArgumentParser()
+arg.add_argument('-o', '--org', required=True, help='Organization name')
+arg.add_argument('-t', '--token', required=True, help='Github token')
+args = vars(arg.parse_args())
+
+output = []
+ORG_NAME = args['org']
+PAT = args['token']
+
+gh = Github(login_or_token=PAT)
+
+org = gh.get_organization(ORG_NAME)
+org.login
+
+for member in org.get_members():
+    user_data = {}
+    user_data['login'] = member.login
+    user_data["name"] = member.name
+    user_data["location"] = member.location
+    user_data["bio"] = member.bio
+    user_data["blog"] = member.blog
+    user_data["avatar"] = member.avatar_url
+    output.append(user_data)
+
+with open(f"_data/members.json.json", "w") as f:
+    json.dump(output, f, indent=4)
