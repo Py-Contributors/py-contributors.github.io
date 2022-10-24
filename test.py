4850838e63a2b77572477c0eeacdbc60cd6993f5
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-24 15:06:25 +0530

    updating members page

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import json
 
-with open('_data\members.json', 'r') as f:
+with open(r'_data\members.json', 'r', encoding="utf-8") as f:
     data = json.load(f)
 
 output = []
-new_dict = {}
 
-for member in data.keys():
-    print(data[member]['name'])
-    new_dict = data[member]
-    new_dict['login'] = member
+
+for member in data:
+    print(member)
+    new_dict = {}
+    new_dict['name'] = member['name']
+    new_dict['login'] = member['login']
+    new_dict['location'] = member['location']
+    new_dict['bio'] = member['bio']
+    new_dict['blog'] = member['blog']
+    new_dict['avatar'] = member['avatar']
     output.append(new_dict)
     
-with open('_data\members.json', 'w') as f:
+    
+with open(r'_data\members.json', 'w') as f:
     json.dump(output, f)
